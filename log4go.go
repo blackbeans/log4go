52d46fd3020fee16ab09cdc5967af20ef25ff2eb
etherealflaim <etherealflaim@localhost>
2010-05-31 21:11:32 +0000

    Changed copyright notice

--- log4go.go
+++ log4go.go
@@ -1,6 +1,4 @@
-// Copyright 2010 The Go Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
+// Copyright (C) 2010, Kyle Lemons <kyle@kylelemons.net>.  All rights reserved.
 
 // Enhanced Logging
 //
