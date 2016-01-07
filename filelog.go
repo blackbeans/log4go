5e15f846ac5bdf7c32d44a9d9acf272862f4967c
赵超 <zhao.chao@immomo.com>
2016-01-07 11:34:49 +0800

    bugfix:加入按日期结尾滚动的时候触发了死锁 	modified:   filelog.go

--- filelog.go
+++ filelog.go
@@ -148,10 +148,10 @@ func (w *FileLogWriter) intRotate() error {
 					fname = w.filename + fmt.Sprintf(".%03d", num)
 					_, err = os.Lstat(fname)
 				}
-			}
-			// return error if the last file checked still existed
-			if err == nil {
-				return fmt.Errorf("Rotate: Cannot find free log number to rename %s\n", w.filename)
+				// return error if the last file checked still existed
+				if err == nil {
+					return fmt.Errorf("Rotate: Cannot find free log number to rename %s\n", w.filename)
+				}
 			}
 
 			// Rename the file to its newfound home
